9518b989acf4d3a6f7458e4a5df15ec4d478dbf7
Dereje1 <getahun.d@gmail.com>
2019-01-02 19:36:40 +0700

    reset ipstack key

--- thirdpartyapis/iptocity.js
+++ thirdpartyapis/iptocity.js
@@ -3,7 +3,7 @@ var axios = require('axios')
 
 module.exports = function(){
   return new Promise(function(resolve,reject){
-    axios.get("http://api.ipstack.com/check?access_key=5d7a1e8efae0998e217954a6707b10c7")
+    axios.get("http://api.ipstack.com/check?access_key="+process.env.IPSTACK)
       .then(function(response){
         resolve(response.data)
       })
